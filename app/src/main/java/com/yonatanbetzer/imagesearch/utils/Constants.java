4b0586519f9170aa050aeed502b5be915092f0e0
Yonatan Betzer <yonatan.betzer@gmail.com>
2018-06-01 11:27:33 +0300

    values per screen width

--- app/src/main/java/com/yonatanbetzer/imagesearch/utils/Constants.java
+++ app/src/main/java/com/yonatanbetzer/imagesearch/utils/Constants.java
@@ -2,11 +2,12 @@ package com.yonatanbetzer.imagesearch.utils;
 
 import android.graphics.Typeface;
 
+import com.yonatanbetzer.imagesearch.R;
 import com.yonatanbetzer.imagesearch.application.ImageSearchApplication;
 
 public class Constants {
     public static int RESULTS_PER_PAGE = 100;
-    public static int IMAGE_HEIGHT = 80;
+    public static int IMAGE_HEIGHT = ImageSearchApplication.getAppContext().getResources().getInteger(R.integer.image_height);
     private static final String OPEN_SANS_REGULAR_HEBREW = "OpenSansHebrew-Regular.ttf";
 
     public static final Typeface openSansRegularHebrew = Typeface.createFromAsset(ImageSearchApplication.getAppContext().getAssets(),Constants.OPEN_SANS_REGULAR_HEBREW);
